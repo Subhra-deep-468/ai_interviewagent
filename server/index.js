ad9f989adce778ef1f939759d7da824ea2f68738
Subhradeep@23 <subhradeepchakraborty738@gmail.com>
2026-05-30 09:10:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"https://ai-interviewagent4.onrender.com",
+    origin:"https://ai-interviewagent6.onrender.com",
     credentials:true
 }))
 
