71258063a3b09e19d6b329acd3c3b57d700ebbc6
Subhra-deep-468 <subhradeepchakraborty738@gmail.com>
2026-05-30 08:41:55 +0530

    new

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -3,11 +3,11 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "interviewiq-ba6ba.firebaseapp.com",
-  projectId: "interviewiq-ba6ba",
-  storageBucket: "interviewiq-ba6ba.firebasestorage.app",
-  messagingSenderId: "862159592601",
-  appId: "1:862159592601:web:7308d702cd708076ddec08"
+  authDomain: "amul-c2846.firebaseapp.com",
+  projectId: "amul-c2846",
+  storageBucket: "amul-c2846.firebasestorage.app",
+  messagingSenderId: "254261454201",
+  appId: "1:254261454201:web:a3f81501e407e1de3c7ba8"
 };
 
 const app = initializeApp(firebaseConfig);
